362acf457312e12b2cb2f3e6ab032d769a614cb0
Your Name <you@example.com>
2022-12-17 12:26:02 -0600

    footer

--- Capstone/src/containers/footer-component.js
+++ Capstone/src/containers/footer-component.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+} from "react-icons/fa";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -8,6 +14,23 @@ const Footer = () => {
     <footer>
       <div>
         <div className="footer-header">
+          <h3>
+            <a href="https://twitter.com/">
+              <FaTwitter />
+            </a>
+            {""}
+            <a href="https://facebook.com/">
+              <FaFacebook />
+            </a>
+            {""}
+            <a href="https://instagram.com/">
+              <FaInstagram />
+            </a>
+            {""}
+            <a href="https://pinterest.com/">
+              <FaPinterest />
+            </a>
+          </h3>
           <h5>parkshelp@parks.gov</h5>
           <h5>312-456-7890</h5>
           <ul className="nav justify-content-center">
